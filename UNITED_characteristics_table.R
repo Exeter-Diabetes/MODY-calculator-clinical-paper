63c66946fd777dda549cbe0cacba3f2c038eab6b
JulieanneKnupp <julieanneknupp@gmail.com>
2024-08-07 14:45:11 +0100

    Reorganised and added sections

--- UNITED_characteristics_table.R
+++ UNITED_characteristics_table.R
@@ -1,14 +1,18 @@
-#UNITED PARTICIPANT CHARACTERISTICS TABLE
+#UNITED PARTICIPANT CHARACTERISTICS TABLE --------------------------------------------
 
 # load libraries
 library(nimble)
 library(rms)
 library(tidyverse)
+library(writexl)
 
-## load functions needed for generating data ----
+## load functions --------------------------------------------
+# needed for generating data 
 source("data/create_data.R")
+# load function needed for generating patient characteristics table
+source("var_characteristics.R")
 
-#load in UNITED type 1 dataset
+#load in UNITED type 1 dataset -----------------------------------------------------
 #M = MODY status (NA = not tested for MODY (i.e. T=1), 1 = tested for MODY (T=0) and positive, 0 = tested for MODY (T=0) and negative)
 #C = c-peptide status (1 = UCPCR >= 0.2; 0 = UCPCR < 0.2)
 #A = antibody status (1 = 1+ positive antibody, 0 = all antibodies tested negative)
@@ -16,8 +20,6 @@ source("data/create_data.R")
 #insoroha = currently treated with insulin or oha (tablets) (1= currently treated with either, 0 = not treated with either)
 #T = biomarker status (1 = cpeptide negative (UCPCR < 0.2) or antibody positive (A =1), 0 = cpeptide positive (UCPCR >=0.2) AND antibody negative (A=0))
 dataset.UNITED_type1 <- create_data(dataset = "united t1d", biomarkers = "full")
-#load in UNITED type 2 dataset
-dataset.UNITED_type2 <- create_data(dataset = "united t2d")
 
 #need to change M=NA to M=0
 dataset.UNITED_type1 <- dataset.UNITED_type1 %>%
@@ -25,9 +27,12 @@ dataset.UNITED_type1 <- dataset.UNITED_type1 %>%
 #checked if worked: should have M=1 (n=7) & M=0 (n=1164)
 table(dataset.UNITED_type1$M)
 
-# load function needed for generating patient characteristics table
-source("var_characteristics.R")
+#load in UNITED type 2 dataset ----------------------------------------------------------
+dataset.UNITED_type2 <- create_data(dataset = "united t2d")
 
+
+#Produce characteristics tables ------------------------------------------------------------
+# for early-insulin-treated ----------------------------------------------------------------
 #create varlist (numeric variables of interest names)
 varlist_T1D = c("agedx","agerec", "bmi", "hba1c")
 #create varlist_cat (categorical variables of interest names)
@@ -37,7 +42,9 @@ var_characteristics(varlist = varlist_T1D, varlist_cat = varlist_cat_T1D, datase
 
 #save as
 UNITED_T1D_table <- as.data.frame(summaryTable_GROUP_missing)
+write_xlsx(UNITED_T1D_table,"UNITED_T1D_table.xlsx")
 
+## for not-early-insulin-treated ----------------------------------------------------------
 #create varlist (numeric variables of interest names)
 varlist_T2D = c("agedx","agerec", "bmi", "hba1c")
 #create varlist_cat (categorical variables of interest names)
@@ -47,9 +54,6 @@ var_characteristics(varlist = varlist_T2D, varlist_cat = varlist_cat_T2D, datase
 
 #save as
 UNITED_T2D_table <- as.data.frame(summaryTable_GROUP_missing)
-
-library(writexl)
-write_xlsx(UNITED_T1D_table,"UNITED_T1D_table.xlsx")
 write_xlsx(UNITED_T2D_table,"UNITED_T2D_table.xlsx")
 
 #Finding the mean MODY prob for M+ AND M- participants
