3837c9c0805198656e4ca2a76cbf310235bca3c3
PM-Cardoso <p.cardoso01@hotmail.com>
2024-09-24 13:00:38 +0100

    Prediction of a MODY gene in UNITED paediatric cases not using C or A information.

--- model_predictions/predictions.R
+++ model_predictions/predictions.R
@@ -304,6 +304,25 @@ predictions_dataset.UNITED_type1_young_all_genes_with_T <- predictions_dataset.U
 saveRDS(predictions_dataset.UNITED_type1_young_all_genes_with_T, "model_predictions/predictions_dataset.UNITED_type1_young_all_genes_with_T.rds")
 
 
+# Type 1 model fitted in UNITED pediatric with all genes 
+### Predictions from new model no T
+interim <- as_tibble(as.matrix(select(dataset.UNITED_type1_young_genes %>% mutate(C = NA, A = NA), pardm, agerec, hba1c, agedx, sex, bmi, C, A)))
+
+predictions_dataset.UNITED_type1_young_all_genes_no_T_full <- predict(posterior_samples_T1D_all_genes_obj, interim, rcs_parms_all_genes)
+
+#### save the predictions
+saveRDS(predictions_dataset.UNITED_type1_young_all_genes_no_T_full, "model_predictions/predictions_dataset.UNITED_type1_young_all_genes_no_T_full.rds")
+
+predictions_dataset.UNITED_type1_young_all_genes_no_T <- predictions_dataset.UNITED_type1_young_all_genes_no_T_full %>%
+  apply(., 2, function(x) {
+    data.frame(prob = mean(x), LCI = quantile(x, probs = 0.025), UCI = quantile(x, probs = 0.975))
+  }) %>%
+  bind_rows() 
+
+#### save the predictions
+saveRDS(predictions_dataset.UNITED_type1_young_all_genes_no_T, "model_predictions/predictions_dataset.UNITED_type1_young_all_genes_no_T.rds")
+
+
 
 
 
@@ -324,7 +343,7 @@ predictions_dataset.UNITED_type1_young_all_genes_old <- predictions_dataset.UNIT
   bind_rows()
 
 for (i in 1:nrow(predictions_dataset.UNITED_type1_young_all_genes_old)) {
-  prob <- predictions_dataset.UNITED_type1_young_all_genes_old[i,]
+  prob <- predictions_dataset.UNITED_type1_young_all_genes_old[i,]predictions_dataset.UNITED_type1_young_all_genes_with_T
   prob <- 10 * ((round(prob  * 100, 0)) %/% 10)
   if (prob == 100) {
     predictions_dataset.UNITED_type1_young_all_genes_old[i,] <- convert$PPVT1[10]
